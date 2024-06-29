4687464470f63536704d7c17a6978031a37ab43e
Makeem <amoshood@africaexchange.com>
2024-06-29 21:40:01 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const user = {
+    name: 'Moshood AKeem Olayinka',
+    username: 'makeem49',
+    email: 'moshoodakeemolayinka@gmail.com'
+};
+
+document.getElementById('name').textContent = user.name;
+document.getElementById('username').textContent = user.username;
+document.getElementById('email').textContent = user.email;
